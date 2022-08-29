0d45cd3c06877df433e41b2d1b2c988d7782f4c0
rong3000 <rong.lin3000@gmail.com>
2022-08-29 13:34:06 +1000

    dotenv introduced and avail balance optimized

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
 const express = require('express')
 const CognitoExpress = require("cognito-express")
 const path = require('path')
@@ -6,16 +9,25 @@ const PORT = process.env.PORT || 3000
 const ethers = require("ethers");//chain specific
 var bigNumber = ethers.BigNumber;//chain specific
 
-const CONTRACT_ID = "0xF3BF07dc4A736f55C8F46711F66dD087d998b3Cf"; //chain specific
+const CONTRACT_ID = process.env.CONTRACT_ID;
 //to be changed after every contract deployed
 
 const Contract = require('./MyToken.json');//chain specific
 
-const url = "http://blockchain.my-neighbours.com:8545/";
+const url = process.env.URL;
 
 const provider = new ethers.providers.JsonRpcProvider(url);//chain specific
 
 let contract = new ethers.Contract(CONTRACT_ID, Contract.abi, provider);//read only; chain specific
+
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  connectionString: process.env.DB_CONNECTION_STR,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const app = express()
   .set('port', PORT)
@@ -37,8 +49,8 @@ app.use(express.json());
 app.use("/api", authenticatedRoute);
 
 const cognitoExpress = new CognitoExpress({
-  region: "ap-southeast-2",
-  cognitoUserPoolId: "ap-southeast-2_XLEKKjhYL",
+  region: process.env.COGNITO_REGION,
+  cognitoUserPoolId: process.env.COGNITO_USER_POOLID,
   tokenUse: "access", //Possible Values: access | id
   tokenExpiration: 3600000 //Up to default expiration of 1 hour (3600000 ms)
 });
@@ -49,20 +61,20 @@ authenticatedRoute.use(function (req, res, next) {
   // if (!accessTokenFromClient) return res.status(401).send("Access Token missing from header");
 
   if (req.headers.authorization && req.headers.authorization.split(" ")[0] === "Bearer") {
-      //I'm passing in the access token in header under key accessToken
-      let accessTokenFromClient = req.headers.authorization.split(" ")[1];
-      cognitoExpress.validate(accessTokenFromClient, function (err, response) {
+    //I'm passing in the access token in header under key accessToken
+    let accessTokenFromClient = req.headers.authorization.split(" ")[1];
+    cognitoExpress.validate(accessTokenFromClient, function (err, response) {
 
-          //If API is not authenticated, Return 401 with error message. 
-          if (err) return res.status(401).send(err);
+      //If API is not authenticated, Return 401 with error message. 
+      if (err) return res.status(401).send(err);
 
-          //Else API has been authenticated. Proceed.
-          res.locals.user = response;
-          next();
-      });
+      //Else API has been authenticated. Proceed.
+      res.locals.user = response;
+      next();
+    });
 
   } else {
-      return res.status(401).send("Access Token missing from header");
+    return res.status(401).send("Access Token missing from header");
   }
 
 });
@@ -87,11 +99,16 @@ async function checkAvail(dbRes, res) {
       // console.log(dbRes.rows[row].trans_hash);
       // console.log(dbRes.rows[row].trans_status);
       if (dbRes.rows[row].trans_hash != null) {
-        if (dbRes.rows[row].trans_status != 0 || dbRes.rows[row].trans_status != 1) {
+        if (dbRes.rows[row].trans_status != 0 && dbRes.rows[row].trans_status != 1) {
 
           let txReceipt = await provider.getTransactionReceipt(dbRes.rows[row].trans_hash);
           if (txReceipt == null) {
             pendingAmount = pendingAmount.add(bigNumber.from(dbRes.rows[row].trans_amount))
+          }
+          if (txReceipt.status == 0 || txReceipt.status == 1) {
+            console.log('mark');
+            updateTXStatus(txReceipt, dbRes.rows[row].trans_hash);
+            
           }
         }
       }
@@ -121,13 +138,42 @@ async function checkAvail(dbRes, res) {
 
 }
 
-async function initializeUser(adminPrivate, userAddress) {
+async function initializeUser(dbRes, adminPrivate, userAddress) {
   let signer = new ethers.Wallet(adminPrivate, provider);
   const tx = await signer.sendTransaction({
     to: userAddress,
     value: ethers.utils.parseEther("0.01")
   });
-  const r = await tx.wait();
+
+  //write tx information into table TRANS_TEST
+  pool.connect((err, client, done) => {
+    if (err) {
+      console.log('err is ', err);
+    }
+
+    client.query("INSERT INTO TRANS_TEST (TRANS_HASH, TRANS_STATUS, TRANS_AMOUNT, USER_ID) VALUES ($1::varchar, $2::int, $3::varchar, $4::int);",
+      [tx.hash,
+        2,
+        "0x2386F26FC10000",
+      dbRes.rows[0].id
+      ], (err, res) => {
+        done()
+        if (err) {
+          console.log(err.stack)
+        } else {
+          // console.log('data inserted into trans db are ', tx.hash,
+          //   2,
+          //   req.body.amount,
+          //   dbRes.rows[0].id);
+          console.log('inserted into Trans db without error', res.command, ' ', res.rowCount);
+        }
+      })
+  })
+
+  const receipt = await tx.wait();
+  //write tx confirmation into table TRANS_TEST
+  updateTXStatus(receipt, tx.hash);
+
 }
 
 async function checkBeforeTransfer(dbResInFunc, res, req, receiver) {
@@ -139,10 +185,20 @@ async function checkBeforeTransfer(dbResInFunc, res, req, receiver) {
 
     for (row in dbResInFunc.rows) {
       if (dbResInFunc.rows[row].trans_hash != null) {
-        if (dbResInFunc.rows[row].trans_status != 0 || dbResInFunc.rows[row].trans_status != 1) {
+        if (dbResInFunc.rows[row].trans_status != 0 && dbResInFunc.rows[row].trans_status != 1) {
+          console.log("dbResInFunc.rows[row] ", dbResInFunc.rows[row]);
           let txReceipt = await provider.getTransactionReceipt(dbResInFunc.rows[row].trans_hash);
           if (txReceipt == null) {
             pendingAmount = pendingAmount.add(bigNumber.from(dbResInFunc.rows[row].trans_amount))
+          }
+          // else {
+          //   updateTXStatus(txReceipt, dbResInFunc.rows[row]);
+
+          // }
+          if (txReceipt.status == 0 || txReceipt.status == 1) {
+            console.log('mark');
+            updateTXStatus(txReceipt, dbResInFunc.rows[row].trans_hash);
+            
           }
         }
       }
@@ -214,18 +270,8 @@ async function contractTransfer(signer, res, req, dbRes, receiver) {
     // console.log('receipt is ', receipt);
 
     //write tx confirmation into table TRANS_TEST
-    let queryText = "UPDATE TRANS_TEST SET TRANS_STATUS = " + "\'" + receipt.status + "\'" + " WHERE TRANS_HASH = " + "\'" + tx.hash + "\'" + ";";
-    pool.connect((err, client, done) => {
-      if (err) throw err
-      client.query(queryText, (err, res) => {
-        done()
-        if (err) {
-          console.log(err.stack)
-        } else {
-          console.log('updated trans_status without error', res.command, ' ', res.rowCount);
-        }
-      })
-    })
+    updateTXStatus(receipt, tx.hash);
+
   } catch (error) {
     console.log(error);
     res.send(JSON.stringify({
@@ -236,22 +282,37 @@ async function contractTransfer(signer, res, req, dbRes, receiver) {
   }
 }
 
+//write tx confirmation into table TRANS_TEST
+function updateTXStatus(receipt, trans_hash) {
+  let queryText = "UPDATE TRANS_TEST SET TRANS_STATUS = " + "\'" + receipt.status + "\'" + " WHERE TRANS_HASH = " + "\'" + trans_hash + "\'" + ";";
+  console.log('q ', queryText);
+  pool.connect((err, client, done) => {
+    if (err) throw err
+    client.query(queryText, (err, res) => {
+      done()
+      if (err) {
+        console.log(err.stack)
+      } else {
+        console.log('updated trans_status without error', res.command, ' ', res.rowCount);
+      }
+    })
+  })
+}
+
+app.get('/', function (req, res) {
+  console.log(process.env.CONTRACT_ID);
+  console.log(process.env.URL);
+  console.log(process.env.COGNITO_USER_POOLID);
+  res.send('Managing user wallets');
+})
+
 authenticatedRoute.get("/", function (req, res, next) {
   const userobj = {
-      "message": `Hi ${res.locals.user.username}, your API call is authenticated!`
+    "message": `Hi ${res.locals.user.username}, your API call is authenticated!`
   }
   res.send(JSON.stringify(
-      userobj
+    userobj
   ));
-});
-
-const { Pool } = require('pg')
-
-const pool = new Pool({
-  connectionString: "postgres://aqnimfmyookfrc:1ed2357a4d5bf2826a3a9822a4d54a97be5bf43acdbe570b6ae9ccdbf3d7942b@ec2-107-22-122-106.compute-1.amazonaws.com:5432/ddrrkuok65vpk5",
-  ssl: {
-    rejectUnauthorized: false
-  }
 });
 
 authenticatedRoute.get("/user", function (req, res, next) {
@@ -288,8 +349,8 @@ authenticatedRoute.get("/user", function (req, res, next) {
 
             client.query("INSERT INTO userwallet5 (username, address, private, balance, availbalance) VALUES ($1::varchar, $2::varchar, $3::varchar, $4::varchar, $5::varchar);",
               [res.locals.user.username,
-                randomWallet.address,
-                randomWallet.privateKey,
+              randomWallet.address,
+              randomWallet.privateKey,
                 '0x0',
                 '0x0'
               ], (err, res) => {
@@ -310,7 +371,7 @@ authenticatedRoute.get("/user", function (req, res, next) {
                 console.log(err.stack)
               } else {
 
-                initializeUser(dbRes.rows[0].private, randomWallet.address);
+                initializeUser(dbRes, dbRes.rows[0].private, randomWallet.address);
 
               }
             })
